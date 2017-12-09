6636a06a70dca39c2f455c010079d1f6a10421fe
Mike D <sprinkles@Mikes-MacBook-Pro.local>
2017-12-09 17:02:20 -0600

    fixed jquery and javascript links in index.html; js gem click events still not working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,32 +16,50 @@
 //if gameScore > gameMatchNum then loss++
 //need to make sure that the value of the gems is different?
 //or at least that 
-$(document).ready(function()){
+$(document).ready(function(){
 
-
+//changed gameMatchNumb, not an array, so setting the variable to the random number
 var gameScore = 0;
-var gameMatchNum = 0;
+var gameMatchNum = Math.floor(Math.random() * 120 + 19);
 var gameWins = 0;
 var gameLosses = 0;
-var gemOneNum = Math.floor((Math.random() * 11) + 1);
-var gemTwoNum = Math.floor((Math.random() * 11) + 1);
-var gemThreeNum = Math.floor((Math.random() * 11) + 1);
-var gemFourNum = Math.floor((Math.random() * 11) + 1);
-console.log(gemOneNum);
+var gemOneNum = Math.floor(Math.random() * 11 + 1);
+var gemTwoNum = Math.floor(Math.random() * 11 + 1);
+var gemThreeNum = Math.floor(Math.random() * 11 + 1);
+var gemFourNum = Math.floor(Math.random() * 11 + 1);
+
 
 //Writing to the html:
-$("#aggScore").text(gameScore);
-$("#matchNumber").text(gameMatchNum);
-$("#aggWins").text(gameWins);
-$("#aggLosses").text(gameLosses);
+$('#aggScore').text(gameScore);
+$('#matchNumber').text(gameMatchNum);
+$('#aggWins').text(gameWins);
+$('#aggLosses').text(gameLosses);
 
 //RESET FUNCTION:
 function reset(){
 	gameMatchNum = Math.floor((Math.random() * 120) + 19);
-	
-	
+	gameScore = 0;
+	gemOneNum = Math.floor((Math.random() * 11) + 1);
+	gemTwoNum = Math.floor((Math.random() * 11) + 1);
+	gemThreeNum = Math.floor((Math.random() * 11) + 1);
+	gemFourNum = Math.floor((Math.random() * 11) + 1);
+
 }
 
+//defining click events on the gems.
+
+$('#gemOne').on('click', function() {
+	gameScore = gameScore + gemOneNum;
+});
+$('#gemTwo').on('click', function() {
+	gameScore = gameScore + gemTwoNum;
+});
+$('#gemThree').on('click', function() {
+	gameScore = gameScore + gemThreeNum;
+});
+$('#gemFour').on('click', function() {
+	gameScore = gameScore + gemFourNum;
+});
 
 
 
